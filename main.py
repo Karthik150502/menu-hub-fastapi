a2b8390f8f727ed2bf474ca1051cb02d417f9e6a
Darshan <vdarshan88@gmail.com>
2026-04-02 16:05:08 +0530

    test

--- main.py
+++ main.py
@@ -5,7 +5,3 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello World", "status":200}
-
-@app.get("/test/")
-def read_root():
-    return {"message": "Testing URL", "status":201}
